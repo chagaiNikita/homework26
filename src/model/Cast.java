6d7caaed2baaa3db8536bd9c55b9e8a3e0211185
chagaiNikita <chagai.nikita@gmail.com>
2024-12-16 17:07:25 +0600

    implemented the compareTo method and also created equals and hashCode

--- src/model/Cast.java
+++ src/model/Cast.java
@@ -1,8 +1,32 @@
 package model;
 
-public class Cast {
+import java.util.Objects;
+
+public class Cast implements Comparable <Cast>{
     private String fullName;
     private String role;
+
+    public Cast(String fullName, String role) {
+        this.fullName = fullName;
+        this.role = role;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Cast cast = (Cast) o;
+        return Objects.equals(fullName, cast.fullName) && Objects.equals(role, cast.role);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(fullName, role);
+    }
+
+    public String getRole() {
+        return role;
+    }
 
     public String getFullName() {
         return fullName;
@@ -10,6 +34,14 @@ public class Cast {
 
     @Override
     public String toString() {
-        return String.format("%nActor: %s Role: %s", fullName, role);
+        return String.format("Actor: %s Role: %s", fullName, role);
+    }
+
+    @Override
+    public int compareTo(Cast o) {
+        if (this.getFullName().equals(o.getFullName())) {
+            return this.getRole().compareTo(o.getRole());
+        }
+        return this.getFullName().compareTo(o.getFullName());
     }
 }
